5b22890a3d6111f338839be85ffbadead2f89290
Matheus Filipe <theuslipe26@gmail.com>
2023-07-30 12:40:32 -0300

    Feat: Implementado novo controler para gerenciar operações

---
+++ API-Produtos/Controllers/ProdutosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Produtos.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ProdutosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult HelthCheck()
+        {
+            return Ok("I'm alive and working");
+        }
+        
+    }
+}
